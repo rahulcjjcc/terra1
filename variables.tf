b0930426a1646480834d6e2eca0d9fe440b5b299
rahulcjjcc <rahuljjcc@gmail.com>
2024-11-26 00:18:34 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "project_id" {
+  description = "GCP project ID"
+  type        = string
+}
+
+variable "region" {
+  description = "Region for the bucket"
+  type        = string
+}
+
+variable "bucket_name" {
+  description = "Name of the bucket"
+  type        = string
+}
